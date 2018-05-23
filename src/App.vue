f1c82b16de277c3ff56da35886ba4daa9849252c
Igor Oliinyk <igor.oleynikdp@gmail.com>
2018-05-23 21:13:03 +0300

    Начальное состояние, убрал компоненты из коробки Установил tailwindcss Добавил компонент цитаты

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container mx-auto">
+    <div class="w-full my-8">
+      <Quote></Quote>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Quote from './components/Quote.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Quote
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="border border-gray p-8 shadow-lg text-center text-5xl font-sans font-light">
+    <p>Классная цитата!</p>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+
+</style>

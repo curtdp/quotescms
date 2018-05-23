3cf27782569f15c087379b157581d2154896ca00
Igor Oliinyk <igor.oleynikdp@gmail.com>
2018-05-23 23:35:31 +0300

    Проверка поведения состояния при динамическом подключении.

--- src/components/New.vue
+++ src/components/New.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="border border-gray p-8 shadow-lg text-center text-5xl font-sans font-light">
-    <h3>Новая цитата</h3>
+    <h3 class="mb-8">Новая цитата</h3>
+    <button @click="counter++" class="py-2 px-4 rounded border border-blue bg-blue hover:bg-blue-dark mr-4 text-white">Увеличить!</button>
+    <p class="mt-4">{{ counter }}</p>
   </div>
 </template>
 
 <script>
   export default {
-
+    data() {
+      return {
+        counter: 0
+      }
+    }
   }
 </script>
 

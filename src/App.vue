639a93162a02b78062a31ba3d62d3b10e1befb6f
Igor Oliinyk <igor.oleynikdp@gmail.com>
2018-05-23 23:25:24 +0300

    Добавили 2 компонента, теперь хотим переключать между тремя Добавили кнопки и состояние для переключения компонентов.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div id="app" class="container mx-auto">
     <div class="w-full my-8">
-      <Quote>
+      <button @click="selectedComponent = 'Quote'"  class="py-2 px-4 rounded border border-blue bg-blue hover:bg-blue-dark mr-4 text-white">Цитата</button>
+      <button @click="selectedComponent = 'Author'" class="py-2 px-4 rounded border border-blue bg-blue hover:bg-blue-dark mr-4 text-white">Автор</button>
+      <button @click="selectedComponent = 'New'"    class="py-2 px-4 rounded border border-blue bg-blue hover:bg-blue-dark mr-4 text-white">Новая</button>
+      <hr class="border border-grey">
+      <div class="py-8">
+        {{ selectedComponent }}
+      </div>
+      <!-- <Quote>
         <h2 slot="title">{{ quoteTitle }}</h2>
         <p>Супер цитата</p>
         <p>Продолжение</p>
-      </Quote>
+      </Quote> -->
     </div>
 
   </div>
@@ -13,15 +20,20 @@
 
 <script>
 import Quote from './components/Quote.vue'
+import Author from './components/Author.vue'
+import New from './components/New.vue'
 export default {
   name: 'app',
   data() {
     return {
-      quoteTitle: 'Умная цитата!'
+      quoteTitle: 'Умная цитата!',
+      selectedComponent: 'Quote'
     }
   },
   components: {
-    Quote
+    Quote,
+    Author,
+    New
   }
 }
 </script>
---
+++ src/components/New.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="border border-gray p-8 shadow-lg text-center text-5xl font-sans font-light">
+    <h3>Новая цитата</h3>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
